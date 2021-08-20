a605156786a6109cab34df29f7cecf8e0d6c19fa
Mohammad Yeasin Arafat Fahim <fahim.std77@gmail.com>
2021-08-20 12:29:24 +0600

    verify match

--- js/app.js
+++ js/app.js
@@ -38,3 +38,21 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
 
 });
 
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+    if (pin == typedNumbers) {
+
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+
+    }
+    else {
+
+        successMessage.style.display = 'none';
+        failError.style.display = 'block';
+    }
+}
+
